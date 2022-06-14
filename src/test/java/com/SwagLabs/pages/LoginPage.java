5b177efdbbb69af39d4598f68133bc52acc9ab47
ASUS <ybalkas@gmail.com>
2022-06-15 00:14:11 +0100

    4 locators found

--- src/test/java/com/SwagLabs/pages/LoginPage.java
+++ src/test/java/com/SwagLabs/pages/LoginPage.java
@@ -1,8 +1,23 @@
 package com.SwagLabs.pages;
 
-public class LoginPage {
- 
+import com.SwagLabs.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.driver(), this);
+    }
+    @FindBy(id="user-name")
+    public WebElement usernameInput;
+    @FindBy(id="password")
+    public WebElement passwordInput;
+    @FindBy(id="login-button")
+    public WebElement loginButton;
+    @FindBy(xpath="//h3[contains(text(),'Epic sadface: Sorry, this user has been locked out.')]")
+    public WebElement errorMessage;
 
 
 }
