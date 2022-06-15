b7fa66c10fb51d28d6175cd660898bb0b158ac3d
yahyayabakan <yahyaayabakan007@gmail.com>
2022-06-15 22:14:50 +0100

    Created Stepdefs

---
+++ src/test/java/com/SwagLabs/stepdefs/LoginPageStepDefs.java
@@ -0,0 +1,24 @@
+package com.SwagLabs.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepDefs {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters username {string} and {string} password")
+    public void user_enters_username_and_password(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should be able to logged in and navigated to products page")
+    public void user_should_be_able_to_logged_in_and_navigated_to_products_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
